6a096a3e1c7e4874204d77084d34cc056e3f0f88
AlexAslam <alexaslam02@hotmail.com>
2018-02-08 13:24:40 +0500

    working on front end deployment

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name augularDeployApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the augularDeployApp
+ */
+angular.module('augularDeployApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name augularDeployApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the augularDeployApp
+ */
+angular.module('augularDeployApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
